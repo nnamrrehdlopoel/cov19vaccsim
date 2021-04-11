9d61eba6d50deb0134a8d747be1cff899415dc1f
Nikita Basargin <nbasargin@web.de>
2021-04-11 19:27:11 +0200

    draw first chart line

--- Dashboard/src/app/components/d3-charts/dummy-chart.component.ts
+++ Dashboard/src/app/components/d3-charts/dummy-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as d3 from 'd3';
+
 import { ChartBase } from './chart-base/chart-base.directive';
 
 export interface DummyChartConfig {
@@ -6,7 +8,7 @@ export interface DummyChartConfig {
 }
 
 export interface DummyChartDataElement {
-    value: number
+    value: number;
 }
 
 @Component({
@@ -14,16 +16,52 @@ export interface DummyChartDataElement {
     templateUrl: 'chart-base/chart-base.directive.html',
     styleUrls: ['chart-base/chart-base.directive.scss'],
 })
-export class DummyChartComponent extends ChartBase<DummyChartConfig, DummyChartDataElement[]> {
+export class DummyChartComponent extends ChartBase<DummyChartConfig, number[]> {
+
+    private xAxis: d3.Selection<SVGGElement, unknown, null, undefined>;
+    private yAxis: d3.Selection<SVGGElement, unknown, null, undefined>;
+    private lines: d3.Selection<SVGGElement, unknown, null, undefined>;
 
     initialChartConfig(): DummyChartConfig {
         return {};
     }
 
-    initializeChart() {
+    initializeChart(): void {
+        this.xAxis = this.svg.append('g').classed('x-axis', true);
+        this.yAxis = this.svg.append('g').classed('y-axis', true);
+        this.lines = this.svg.append('g').classed('lines', true);
     }
 
     updateChart(): void {
+        const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+        const dataMax = d3.max(this.data);
+        const dataMin = d3.min(this.data);
+        const x = d3
+            .scaleLinear()
+            .domain([0, this.data.length - 1])
+            .range([
+                margin.left,
+                this.chartSize.width - margin.right,
+            ]);
+        const y = d3
+            .scaleLinear()
+            .domain([dataMin, dataMax])
+            .range([this.chartSize.height - margin.bottom, margin.top]);
+
+        const lineGenerator: d3.Line<number> = d3
+            .line<number>()
+            .defined((d) => d != null)
+            .x((d, i) => x(i))
+            .y((d) => y(d));
+
+        this.lines
+            .selectAll('path')
+            .data([this.data])
+            .join('path')
+            .attr('fill', 'none')
+            .attr('stroke', 'black')
+            .attr('stroke-width', '2')
+            .attr('d', (d) => lineGenerator(d));
     }
 
 }
--- Dashboard/src/app/pages/playground-page/playground-page.component.ts
+++ Dashboard/src/app/pages/playground-page/playground-page.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { DummyChartDataElement } from '../../components/d3-charts/dummy-chart.component';
 
 @Component({
-  selector: 'app-playground-page',
-  templateUrl: './playground-page.component.html',
-  styleUrls: ['./playground-page.component.scss']
+    selector: 'app-playground-page',
+    templateUrl: './playground-page.component.html',
+    styleUrls: ['./playground-page.component.scss']
 })
 export class PlaygroundPageComponent {
-  data: DummyChartDataElement[] = [
-    {value: 42},
-  ];
+    data: number[] = [
+        1, 2, 3, 4, 1, 2, 5, 2, 3, 4
+    ];
 }
