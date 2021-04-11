ef0ef815a6f5c0cff405d5e079c04d0becb5b8dd
Daniel <7558512+DerGenaue@users.noreply.github.com>
2021-04-12 01:30:35 +0200

    Add first playground functionality & current vaccinations data

--- Dashboard/src/app/pages/playground-page/playground-page.component.ts
+++ Dashboard/src/app/pages/playground-page/playground-page.component.ts
@@ -1,14 +1,80 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { DummyChartData } from '../../components/d3-charts/dummy-chart.component';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
+import {CsvexportService} from '../../services/csvexport.service';
+import * as d3 from 'd3';
 
 @Component({
     selector: 'app-playground-page',
     templateUrl: './playground-page.component.html',
     styleUrls: ['./playground-page.component.scss']
 })
-export class PlaygroundPageComponent {
+export class PlaygroundPageComponent implements OnInit {
+
+    constructor(
+        private http: HttpClient,
+        private route: ActivatedRoute,
+        private csv: CsvexportService) { }
+
+
     data: DummyChartData = {
         vacData: [1, 2, 3, 4, 1, 2, 5, 2, 3, 4],
         vacStart: new Date(2021, 0, 1),
     };
+    vaccinations: d3.DSVParsedArray<{
+        date: Date,
+        dosen_kumulativ: number,
+        dosen_astrazeneca_kumulativ: number,
+        dosen_biontech_kumulativ: number,
+        dosen_differenz_zum_vortag: number,
+        dosen_erst_differenz_zum_vortag: number,
+        dosen_moderna_kumulativ: number,
+        dosen_zweit_differenz_zum_vortag: number,
+        impf_quote_erst: number,
+        impf_quote_voll: number,
+        indikation_alter_dosen: number,
+        indikation_alter_erst: number,
+        indikation_alter_voll: number,
+        indikation_beruf_dosen: number,
+        indikation_beruf_erst: number,
+        indikation_beruf_voll: number,
+        indikation_medizinisch_dosen: number,
+        indikation_medizinisch_erst: number,
+        indikation_medizinisch_voll: number,
+        indikation_pflegeheim_dosen: number,
+        indikation_pflegeheim_erst: number,
+        indikation_pflegeheim_voll: number,
+        personen_erst_kumulativ: number,
+        personen_voll_kumulativ: number,
+    }>;
+    deliveries: d3.DSVParsedArray<{
+        date: Date,
+        dosen: number,
+        impfstoff: string,
+        region: string
+    }>;
+
+    ngOnInit(): void {
+        window.scrollTo(0, 0);
+        this.loaddata();
+    }
+
+    loaddata(): void {
+        this.http.get('/assets/data/germany_vaccinations_timeseries_v2.tsv', {responseType: 'text'})
+            .subscribe(data => {
+                // @ts-ignore
+                this.vaccinations = d3.tsvParse(data, d3.autoType);
+                this.data.vacStart = this.vaccinations[0].date;
+                this.data.vacData = this.vaccinations.map(x => x.dosen_kumulativ);
+                // TODO: update chart?
+                console.log(this.vaccinations);
+            });
+        this.http.get('/assets/data/germany_deliveries_timeseries_v2.tsv', {responseType: 'text'})
+            .subscribe(data => {
+                // @ts-ignore
+                this.deliveries = d3.tsvParse(data, d3.autoType);
+                console.log(this.deliveries);
+            });
+    }
 }
